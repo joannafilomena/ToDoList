e60cef504a9238537d63f5a395ab4d2f76c70d71
Joanna Kaminska <joannafilomena@gmail.com>
2024-08-23 14:42:27 +0200

    task status added

--- js/script.js
+++ js/script.js
@@ -11,15 +11,40 @@
     const render = () => {
         let HTMLString = "";
         for (const task of tasks) {
-            HTMLString += `<li class="list__task ${task.done ? "list__task--done" : ""}">${task.content}</li>`
+            HTMLString += `<li class="list__task ${task.done ? "list__task--done" : ""}"><button class="list__button--done">Done</button>${task.content}<button class="list__button--remove">Remove</button></li>`
         };
 
         document.querySelector(".list").innerHTML = HTMLString;
-    }
+
+        const removeButtons = document.querySelectorAll(".list__button--remove");
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
+        });
+
+        const doneButtons = document.querySelectorAll(".list__button--done");
+        doneButtons.forEach((doneButton, index) => {
+            doneButton.addEventListener("click", () => {
+                changeTaskStatus(index);
+            })
+        })
+    };
+
     const addNewTask = (newTask) => {
         tasks.push({
             content: newTask,
         });
+        render();
+    }
+
+    const removeTask = (index) => {
+        tasks.splice(index, 1);
+        render();
+    }
+
+    const changeTaskStatus = (index) => {
+        tasks[index].done = !tasks[index].done;
         render();
     }
 
